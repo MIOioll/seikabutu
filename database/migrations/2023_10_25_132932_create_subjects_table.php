aa6e39f63579a18fc9ed9a5f9ea98ef1930eb9d8
MIOioll <22111068mo@tama.ac.jp>
2023-10-25 13:45:07 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2023_10_25_132932_create_subjects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+             $table->string('name',30);
+             $table->string('professer_name',20);
+            $table->string('summary',2000);
+            $table->string('evaluation',2000);
+            $table->string('test',1000);
+            $table->string('category',10);
+            $table->string('stars_average',5);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
